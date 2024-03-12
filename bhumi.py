61e89904fe5ad865f5639f521b7bc0c59df95048
B-humi <159120274+B-humi@users.noreply.github.com>
2024-03-12 13:36:26 +0530

    Update bhumi.py

--- bhumi.py
+++ bhumi.py
@@ -1,17 +1,8 @@
-# Python Program to find the area of triangle
+# Python Program to find the area of rectangle
 
-a = 5
-b = 6
-c = 7
+a = 6
+b = 2
 
-# Uncomment below to take inputs from the user
-# a = float(input('Enter first side: '))
-# b = float(input('Enter second side: '))
-# c = float(input('Enter third side: '))
+area=a*b
 
-# calculate the semi-perimeter
-s = (a + b + c) / 2
-
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+print('The area of the rectangle is + str(area))
